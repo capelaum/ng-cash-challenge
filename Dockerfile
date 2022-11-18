de64778752f5544051d007f954856b909af17947
Luis V. Capelletto <thecapellett@gmail.com>
2022-11-18 13:03:48 -0300

    🔧 config docker

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM node:18.12
+
+WORKDIR /usr/src/app
+
+COPY package.json yarn.lock ./
+RUN yarn install
+
+COPY . .
+
+EXPOSE 3333
+CMD [ "yarn", "dev" ]
--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ const app = express()
 app.use(morgan('dev'))
 
 app.get('/', (req, res) => {
-  res.json({ message: 'Hello World!' })
+  res.json({ message: 'Hello teste!!!' })
 })
 
 const port = Number(process.env.PORT ?? 3333)
